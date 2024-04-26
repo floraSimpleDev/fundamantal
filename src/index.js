78d8cba936ba8c58d0a4c94d10f5122a99f88d24
Flora Li <samantha20200604@gmail.com>
2024-04-26 20:35:00 +1000

    first simple props example

--- src/index.js
+++ src/index.js
@@ -1,28 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/* import './index.css';
-import App from './App';
+import './index.css';
+/* import './images/book-1.jpg'; */
+/* import App from './App';
 import reportWebVitals from './reportWebVitals'; */
 
-function Greeting() {
+
+const firstBook = {
+  author: 'Laura Nowlin',
+  title: 'If Only I Had Told Her',
+  image: './images/book-1.jpg',
+};
+
+const secondBook = {
+  author: 'Steven Carroll',
+  title: 'Death of a Foreign Gentleman',
+  image: './images/book-2.jpg',
+};
+
+function BookList() {
   return (
-  <>
-    <Person />
-    <Message />
-  </>
+    <section className='booklist'>
+      <Book 
+        author = {firstBook.author}
+        title = {firstBook.title}
+        image = {firstBook.image}
+      />
+      <Book
+        author = {secondBook.author}
+        title = {secondBook.title}
+        image = {secondBook.image}
+      />
+      {/* <Book />
+      <Book />
+      <Book />
+      <Book /> */}
+    </section>
   );
 }
 
-const Person = () => <h2>john doe</h2>
-const Message = () => {
-  return <p>this is my message</p>
+const Book = (props) => {
+  return (
+    <article className='book'>
+      <img src= {props.image} alt= {props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+    </article>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* const Author = () => {
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+    letterSpacing: '.125rem',
+  };
+  return <h4 style={inlineHeadingStyles}></h4>;
+}; */
 
-root.render(
-  <Greeting/>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
