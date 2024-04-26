a540bccb9df08f0a6f99e2b390e6ea294b6cf840
Flora Li <samantha20200604@gmail.com>
2024-04-26 11:20:38 +1000

    my first component Greeting, Person and Message

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+/* import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; */
+
+function Greeting() {
+  return (
+  <>
+    <Person />
+    <Message />
+  </>
+  );
+}
+
+const Person = () => <h2>john doe</h2>
+const Message = () => {
+  return <p>this is my message</p>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Greeting/>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
